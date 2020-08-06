885419a594ac8458c3f66273177407c07e35e7f4
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-08-06 21:09:04 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 import React from "react"
-import Checkbox from "./components/Checkbox"
+import ToDoItem from "./ToDoItem"
+
 
 function App(){
     return(
-<div>
-<input type="checkbox" id = "todo"/>
-<label htmlFor="todo">TODO</label>
-<br></br>
-<input type="checkbox" id = "todoa"/>
-<label htmlFor="todoa">TODO</label>
-<br></br>
-<input type="checkbox" id = "todob"/>
-<label htmlFor="todob">TODO</label>
-<br></br>
+<div className = "todo-list">
+<ToDoItem />
+<ToDoItem />
+<ToDoItem />
+<ToDoItem />
 </div>
     )
 }
