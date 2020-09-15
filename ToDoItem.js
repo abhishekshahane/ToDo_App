24b616f4d65c4ce133a8139d3eb7f8bccd353da2
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-09-15 18:30:25 +0530

    Update ToDoItem.js

--- ToDoItem.js
+++ ToDoItem.js
@@ -1,10 +1,12 @@
+//BE WARNED!
+//This does throw a error, but you can see the results.
 import React from "react"
 
-function TodoItem() {
+function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" />
-            <p>Placeholder text here</p>
+            <input type="checkbox" checked={props.product.completed}/>
+            <p>{props.product.text}</p>
         </div>
     )
 }
